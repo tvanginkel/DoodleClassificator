182a1535bb31a259237ebe49eaecaf908c6a04cf
Toni <tginkel1@gmail.com>
2020-10-25 13:36:14 +0000

    Dividing data in test set and training sets

--- DoodleClassificator/sketch.js
+++ DoodleClassificator/sketch.js
@@ -1,14 +1,19 @@
-let pianos;
-let knifes;
-let cars;
+const len = 784;
+const total_data = 1000;
 
-//console.log("Hello")
+let pianos_data;
+let knifes_data;
+let cars_data;
+
+let pianos = {};
+let knifes = {};
+let cars = {};
 
 function preload() 
 {
-    pianos = loadBytes('data/pianos1000.bin');
-    knifes = loadBytes('data/knifes1000.bin');
-    cars = loadBytes('data/cars1000.bin');
+    pianos_data = loadBytes('data/pianos1000.bin');
+    knifes_data = loadBytes('data/knifes1000.bin');
+    cars_data = loadBytes('data/cars1000.bin');
 }
 
 function setup()
@@ -16,15 +21,32 @@ function setup()
     createCanvas(280,280);
     background(100);
 
-    let total = 100;
+    pianos.training = [];
+    pianos.testing = [];
+    let threshold = floor( 0.8 * total_data);
+
+    for (let i = 0; i < total_data; i++)
+    {
+        let offset = i * len;
+        if (i < threshold)
+        {   
+            pianos.training[i] = pianos_data.bytes.subarray(offset, offset + len);
+        }
+        else
+        {
+            pianos.testing[i - threshold] = pianos_data.bytes.subarray(offset, offset + len);
+        }
+    }
+/*    let total = 100;
     for (let n = 0; n < total; n++)
     {
         let img = createImage(28,28);
         img.loadPixels();
         let offset = n * 784;
+
         for (let i = 0; i < 784; i++)
         {
-            let val = knifes.bytes[i + offset];
+            let val =  255 - cars_data.bytes[i + offset];
             img.pixels[i * 4 + 0] = val;
             img.pixels[i * 4 + 1] = val;
             img.pixels[i * 4 + 2] = val;
@@ -36,5 +58,5 @@ function setup()
         let y = floor(n / 10) * 28;
 
         image(img,x,y);
-    }
+    }*/
 }
